075fc3aa0dec89888a8fb8e1159eeb293411a83e
jfranco7156 <jfranco7156@bths.edu>
2016-09-14 13:54:43 -0400

    Added a new line

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -3,5 +3,6 @@ package introduction;
 public class ProceduralExample {
   public static void main(String[] args){
     System.out.println("Hello World! I am coding in JAVA!");
+    System.out.println("I am going to learn Java this year");
   }
 }
