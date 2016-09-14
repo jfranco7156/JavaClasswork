f4951d3c893e2a9bf25f934942ffa7c43edd6c21
jfranco7156 <jfranco7156@bths.edu>
2016-09-14 13:58:12 -0400

    Update ProceduralExample.java

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -2,7 +2,7 @@ package introduction;
 
 public class ProceduralExample {
   public static void main(String[] args){
-    System.out.println("Hello World! I am coding in JAVA!");
+    System.out.println("Hello World! I am coding in JAVA! First time");
     System.out.println("I am going to learn Java this year");
   }
 }
