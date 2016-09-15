a63e2c100474e5fc8583809141a2ce7756ab2522
Jenniber Franco <jfranco7156@bths.edu>
2016-09-15 01:32:14 -0400

    Changes made at home

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -3,6 +3,7 @@ package introduction;
 public class ProceduralExample {
   public static void main(String[] args){
     System.out.println("Hello World! I am coding in JAVA for the first time! YAY!");
+    System.out.println("I am coding in JAVA.");
 
   }
 }
