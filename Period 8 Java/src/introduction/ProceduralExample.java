7141be8d0dbb2d6e18c01c9fbd67e56d177073f1
jfranco7156 <jfranco7156@bths.edu>
2016-09-15 13:53:14 -0400

    Added a for loop

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -3,6 +3,9 @@ package introduction;
 public class ProceduralExample {
   public static void main(String[] args){
 	  String[] names = {"Jillian","Joseph","Jordan"};
-	  System.out.println("Hi, my name is " + names[0] + ".");
+	  String[] years = {"Sophomore", "Junior", "Senior"};
+	  for(int x=0; x<3; x++){
+	  	System.out.println("Hi, my name is " + names[x] + "and I am" + years[x]);
+	  }
   }
 }
