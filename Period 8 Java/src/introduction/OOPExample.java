2a16b2b66f7a89f2bd57e45e22ca1e999cf067ea
Student 8 <Student 8@10.8.33.159>
2016-09-16 13:10:59 -0400

    Made an Object-Oriented Program Example and a Student class

---
+++ Period 8 Java/src/introduction/OOPExample.java
@@ -0,0 +1,16 @@
+package introduction;
+
+public class OOPExample {
+
+	public static void main(String[] args) {
+		//Use 'new' to refer to a constructor
+		Student jillian = new Student("Jillian");
+		Student josh = new Student("Josh");
+		Student jordan = new Student("Jordan");
+		
+		jillian.talk();
+		josh.talk();
+		jordan.talk();
+	}
+
+}
---
+++ Period 8 Java/src/introduction/Student.java
@@ -0,0 +1,19 @@
+package introduction;
+
+public class Student {
+	
+	//fields
+	private String name;
+	
+	//constructor - return type is the name
+	//constructor MUST ALWAYS match the class name
+	//constructor initializes fields 
+	public Student(String name){
+		this.name = name;
+	}
+	
+	//method
+	public void talk(){
+		System.out.println("Hello, my name is " + name+".");
+	}
+}
