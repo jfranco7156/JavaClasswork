1144b54650b7c014cff50eb00851faed8bd9aa51
Student 8 <Student 8@10.8.33.159>
2016-09-21 13:29:37 -0400

    Edited String Line

--- Period 8 Java/src/introduction/RandomDrawCard.java
+++ Period 8 Java/src/introduction/RandomDrawCard.java
@@ -5,13 +5,15 @@ public class RandomDrawCard {
 	public static void main(String[] args) {
 		
 		String[] deck = {"2","3","4","5","6","7","8","9","Jack","Queen","King","Ace"};
-		String[] suit = {"Diamond","Clubs","Heart","Spade"};
+		String[] suit = {"Diamonds","Clubs","Hearts","Spades"};
 		
-		for(int num=0; num<10; num++){
+		int drawNum = 10;
+		
+		for(int num=0; num<drawNum; num++){
 			int randDeck = getArrayNum(12);
 			int randSuit = getArrayNum(4);
 			
-			System.out.println("The "+suit[randSuit]+" of "+deck[randDeck]);
+			System.out.println("The "+deck[randDeck]+" of "+suit[randSuit]);
 		}
 		
 	}
