78049b606c57b2d9c542dfd33fea2940b8b3532e
Student 8 <Student 8@10.8.33.159>
2016-09-23 14:00:10 -0400

    Break Line Method Fixed

--- Period 8 Java/src/introduction/StringPractice.java
+++ Period 8 Java/src/introduction/StringPractice.java
@@ -6,19 +6,19 @@ public class StringPractice {
 
 	static Scanner input;
 	static String user;
-	
+
 	public static void main(String[] args) {
 		//demonstrateStringMethods();
 		createFields();
 		promptName();
 		promptForever();
 	}
-	
+
 	public static void promptName() {
 		print("Enter your name.");
 		user = input.nextLine();
 		print("Glad to meet you, "+user+". For the rest of time, I will call you "+user+". You may call me: Computer. We should become friends.");
-		
+
 	}
 
 	public static void promptForever() {
@@ -31,56 +31,82 @@ public class StringPractice {
 		print("Please type something "+user+".");
 		String userInput = input.nextLine();
 		print("Congratulations! You typed: "+userInput);
-		
+
 	}
 
 	public static void createFields(){
 		input = new Scanner(System.in);
 		user = "";
 	}
-	
-	
-	
+
+
+
 	public static void demonstrateStringMethods(){
 		String text1 = new String("Hello World");
 		String text2 = "Hello World";// same as above
-		
+
 		//ALL objects get prepared using the ".equals" method
 		if(text1.equals(text2)){
 			print("These strings are equal: ");
 		}
 		print(text1);
 		print(text2);
-		
+
 		String word1 = "Aardvark";
 		String word2 = "Zyzzyva";
-		
+
 		if(word1.compareTo(word2) < 0){
 			print("word1 comes before word2");
 		}
 	}
-	
-	public static void print(String m){
-		String printString = m;
-		int cutoff = 45;
-		if(printString.length()>cutoff){
-			for(int i = 0; i*cutoff<m.length(); i++){
-				// append +=
-				printString += getCut(m,cutoff, i+1)+"\n";
+
+	public static void print(String s){
+		String printString = "";
+		int cutoff = 25;
+		//this while loop last as long as there are words left in the original String
+		while(s.length() > 0){
+
+			String currentCut = "";
+			String nextWord = "";
+
+			//while the current cut is still less than the line length 
+			//AND there are still words left to add
+			while(currentCut.length()+nextWord.length() < cutoff && s.length() > 0){
+
+				//add the next word
+				currentCut += nextWord;
+
+				//remove the word that was added from the original String
+				s = s.substring(nextWord.length());
+
+				//identify the following word, exclude the space
+				int endOfWord = s.indexOf(" ");
+
+				//if there are no more spaces, this is the last word, so add the whole thing
+				if(endOfWord == -1) {
+					endOfWord = s.length()-1;//subtract 1 because index of last letter is one less than length
+				}
+
+				//the next word should include the space
+				nextWord = s.substring(0,endOfWord+1);
 			}
+
+			printString +=currentCut+"\n";
+
 		}
-		
-		System.out.println(printString);
+		System.out.print(printString);
 	}
-	
+
+
+	/*	
 	private static String getCut(String m, int cutoff, int cut){
 		int cutIndex = cut*cutoff;
 		if(cutIndex>m.length()){
 			cutIndex= m.length();
 		}
-		
+
 		String currentCut = m.substring(0, cutIndex);
-		
+
 		int indexOfLastSpace = currentCut.length()-1;
 		//Start at last index, go backwards
 		for(int i = currentCut.length()-1; i>=0; i--){
@@ -90,10 +116,10 @@ public class StringPractice {
 				break;
 			}
 		}
-		
+
 		//shorten the cut to end on a space
 		currentCut = currentCut.substring(0,indexOfLastSpace);
 		return currentCut;
 	}
-
+	 */
 }
