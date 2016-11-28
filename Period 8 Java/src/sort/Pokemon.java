dcca50cf776d6414655fde56a2344bc865adf6ea
Student 8 <Student 8@10.8.33.159>
2016-11-28 14:03:25 -0500

    Pokemon!!!

---
+++ Period 8 Java/src/sort/Pokemon.java
@@ -0,0 +1,66 @@
+package sort;
+
+public class Pokemon {
+	private int level;
+	private int hp;
+	private String name;
+	private boolean poisoned;
+	
+	public void attack(Pokemon target, Attack attack){
+		if(Math.random()<.9){
+			attack.attack(target);
+			System.out.println("The attack hit!");
+		}else{
+			System.out.println("The attack missed!");
+		}
+	}
+	
+	public void iChooseYou(){
+		System.out.println(name+", "+name);
+	}
+	
+	public void setValues(String name,int level){
+		setName(name);
+		setLevel(level);
+		setHp(100);
+		setPoisoned(false);
+	}
+	
+	public void lapse(){
+		if(poisoned){
+			hp-=15;
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public boolean isPoisoned() {
+		return poisoned;
+	}
+
+	public void setPoisoned(boolean poisoned) {
+		this.poisoned = poisoned;
+	}
+}
