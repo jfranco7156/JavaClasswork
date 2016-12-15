56fa4e84daaffbf13d18d7d24d6f0de5175fd114
Student 8 <Student 8@10.8.33.159>
2016-12-15 14:03:45 -0500

    Images

--- Period 8 Java/src/guiPractice/components/Graphic.java
+++ Period 8 Java/src/guiPractice/components/Graphic.java
@@ -10,6 +10,18 @@ public class Graphic implements Visible {
 	private BufferedImage image;
 	private boolean loadedImages;
 	
+	public Graphic(int x, int y, String imageLocation){
+		this.x = x;
+		this.y = y;
+		loadedImages = false;
+		loadImages(imageLocation, 0.0);
+	}
+	
+	private void loadImages(String imageLocation, double d) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	@Override
 	public BufferedImage getImage() {
 		// TODO Auto-generated method stub
