06db39c0b969ffa80a2f596e31e9188ffe66c7ed
Student 8 <Student 8@10.8.33.159>
2016-12-21 13:34:44 -0500

    up

--- Period 8 Java/src/guiPractice/sampleGames/ClickGraphicsGame.java
+++ Period 8 Java/src/guiPractice/sampleGames/ClickGraphicsGame.java
@@ -3,18 +3,25 @@ package guiPractice.sampleGames;
 import guiPractice.GUIApplication;
 
 public class ClickGraphicsGame extends GUIApplication {
+	
+	public static ClickableScreen screen;
+	public static ClickGraphicsGame game;
 
 	public ClickGraphicsGame() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[]args){
-		ClickGraphicsGame
-	}
-	
 	@Override
 	protected void initScreen() {
-		// TODO Auto-generated method stub
+		screen = new ClickableScreen(getWidth(),
+				getHeight());
+		setScreen(screen);
+	}
+
+	public static void main(String[] args) {
+		game = new ClickGraphicsGame();
+		Thread app = new Thread(game);
+		app.start();
 
 	}
 
