127ed0ce83929fa1613d04beb28643c2e2d0d7f7
Student 8 <Student 8@10.8.33.159>
2017-01-05 13:23:04 -0500

    Simon Game Code WIP

--- Period 8 Java/src/guiPractice/Simon/ButtonInterfaceJenniber.java
+++ Period 8 Java/src/guiPractice/Simon/ButtonInterfaceJenniber.java
@@ -1,7 +1,20 @@
 package guiPractice.Simon;
 
+import java.awt.Color;
+
+import guiPractice.components.Action;
 import guiPractice.components.Clickable;
 
 public interface ButtonInterfaceJenniber extends Clickable {
+
+	void setAction(Action action);
+	
+	void setColor(Color color);
+
+	void setX(int i);
+
+	void setY(int i);
+
+	void highlight();
 
 }
--- Period 8 Java/src/guiPractice/Simon/SimonScreenJenniber.java
+++ Period 8 Java/src/guiPractice/Simon/SimonScreenJenniber.java
@@ -1,20 +1,30 @@
 package guiPractice.Simon;
 
+import java.awt.Color;
 import java.util.ArrayList;
 
+import guiPractice.components.Action;
+import guiPractice.components.Button;
+import guiPractice.components.TextLabel;
 import guiPractice.components.Visible;
 import guiPractice.sampleGames.ClickableScreen;
 
 public class SimonScreenJenniber extends ClickableScreen implements Runnable {
 
-	public ProgressInterfaceJenniber progress;
-	private ArrayList<MoveInterfaceJenniber> movables;
-	public int roundNum;
-	public boolean userInput;
+	private TextLabel label;
+	private ButtonInterfaceJenniber[] button;
+	private ProgressInterfaceJenniber progress;
+	private ArrayList<MoveInterfaceJenniber> sequence;
+	
+	private int roundNumber;
+	private boolean acceptingInput;
+	private int sequenceIndex;
+	private int lastSelectedButton;
 	
 	public SimonScreenJenniber(int width, int height) {
 		super(width, height);
-		// TODO Auto-generated constructor stub
+		Thread app = new Thread(this);
+		app.start();
 	}
 
 	@Override
@@ -25,8 +35,75 @@ public class SimonScreenJenniber extends ClickableScreen implements Runnable {
 
 	@Override
 	public void initAllObjects(ArrayList<Visible> viewObjects) {
-		// TODO Auto-generated method stub
+		addButtons();
+		progress = getProgress();
+		label = new TextLabel(130,230,300,40,"Let's play Simon!");
+		sequence = new ArrayList<MoveInterfaceJenniber>();
+		//add 2 moves to start
+		lastSelectedButton = -1;
+		sequence.add(randomMove());
+		sequence.add(randomMove());
+		roundNumber = 0;
+		viewObjects.add(progress);
+		viewObjects.add(label);
+	}
 
+	private MoveInterfaceJenniber randomMove() {
+		ButtonInterfaceJenniber b = null;
+		int randNum = (int)(Math.random()*button.length);
+		while(randNum==lastSelectedButton){
+			randNum = (int)(Math.random()*button.length);
+		}
+		b = button[randNum];
+		lastSelectedButton = randNum;
+		return getMove(b);
+	}
+
+	public MoveInterfaceJenniber getMove(ButtonInterfaceJenniber b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ProgressInterfaceJenniber getProgress() {
+		// Wait for partner's code
+		return null;
+	}
+
+	public void addButtons() {
+		int numOfButtons = 6;
+		Color[] colors= {Color.blue,Color.red,Color.magenta, Color.yellow, 
+				Color.green, Color.orange};
+		for(int i= 0; i<numOfButtons; i++){
+			final ButtonInterfaceJenniber b = getAButton();
+			b.setColor(colors[i]);
+			b.setX(50);
+			b.setY(50);
+			b.setAction(new Action(){
+				public void act() {
+					if(acceptingInput){
+						Thread blink = new Thread(new Runnable(){
+							public void run() {
+								b.highlight();
+								try {
+									Thread.sleep(800);
+								} catch (InterruptedException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+							}
+							
+						});
+					}
+				}
+				
+			});
+		}
+		
+	}
+
+	public ButtonInterfaceJenniber getAButton() {
+		//get partner's code
+		return null;
 	}
 
 }
